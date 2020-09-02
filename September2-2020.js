04dde397c1aab7046867ffb3c931f90bc4754287
crossar <41621244+crossar@users.noreply.github.com>
2020-09-02 14:32:55 -0400

    Update September2-2020.js
    Shows how to comment js code and how to do addition & substraction using js

--- September2-2020.js
+++ September2-2020.js
@@ -8,3 +8,19 @@ veggie = "cucumber";
 drink = "I love Boba!";
 
 console.log(fruit, veggie, drink)
+
+/* This is an in-line comment
+show how to do addition 
+
+*/
+
+var total = 100 + 65 + 26;
+console.log(total)
+
+
+// this is another example that shows how to do substraction
+
+var difference = 167 - 6782;
+console.log(difference)
+
+
