67a55ddebab6c71db8d15b144a16511777885a0f
crossar <41621244+crossar@users.noreply.github.com>
2020-09-04 14:47:55 -0400

    Update September4-2020.js
    Compound Assignment with Augmented Subtraction, Multiplication, Division and Declare String Variables

--- September4-2020.js
+++ September4-2020.js
@@ -16,5 +16,56 @@ a += 22;
 b += 6;
 c += 3;
 
-console.log(a,b,c);
+console.log(a,b,c)
+
+// Compound Assignment with Augmented Subtraction
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// answer should be 5 -6 2
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+console.log(a,b,c)
+
+// Compound Assignment With Augmented Multiplication
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// answer should be 25 36 46
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+// Compound Assignment With Augmented Division
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+//  answer should be  4 27 3
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+console.log(a,b,c)
+
+// Declare String Variables
+
+var myFirstName = "Aileen";
+var myLastName = "Cross";
+
+console.log(myFirstName, myLastName)
+
+
+
+
 
