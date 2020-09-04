cc00b0030c365d2b5a8bff25b4a01b9150098686
crossar <41621244+crossar@users.noreply.github.com>
2020-09-04 14:25:25 -0400

    Create September4-2020.js
    Finding Remainder & Compound Assignment with Augmented Addition

---
+++ September4-2020.js
@@ -0,0 +1,20 @@
+
+//Finding remainder
+
+var remainder = 17 % 2;
+console.log(remainder)
+
+// Compound Assignment with Augmented Addition
+
+var a = 5;
+var b = 3;
+var c = 7;
+
+//answer should be 27(myAge), (9)month of birth (10)day of birth :)
+
+a += 22;
+b += 6;
+c += 3;
+
+console.log(a,b,c);
+
