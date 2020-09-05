87a42f9fc523a946eafecd4de5dffa8921780796
crossar <41621244+crossar@users.noreply.github.com>
2020-09-05 17:45:40 -0400

    Create September5-2020.js
    Escape Sequences in Strings

---
+++ September5-2020.js
@@ -0,0 +1,17 @@
+//Escape Sequences in Strings
+
+/* here are the things you have to keep in mind.
+
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\r	carriage return
+\t	tab
+\b	word boundary
+\f	form feed
+
+*/
+
+var myDog = "Peter\n\t\\Cross\n'TheWiener'";
+console.log(myDog)
