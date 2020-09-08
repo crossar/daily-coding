668c27270479496405267145fbbbc0f42fe27846
crossar <41621244+crossar@users.noreply.github.com>
2020-09-08 19:10:24 -0400

    Update September8-2020.js
     Access Array Data with Indexes

--- September8-2020.js
+++ September8-2020.js
@@ -7,3 +7,12 @@ var myArray = ["Hershey", 223, "Toblerone", 121 ];
 // Nest one Array within Another Array
 
 var myArray = [["Isay", 20], ["Isoy", 18]];
+
+// Access Array Data with Indexes
+
+var myArray = [50,60,70];
+
+// the myData should be equal to first value of myArray
+var myData = [50,55,62];
+myArray[0];
+var myData = myArray[0];
