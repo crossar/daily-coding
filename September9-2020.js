30364a536c5cb23ae531c9c90ccd10d49f0087a2
crossar <41621244+crossar@users.noreply.github.com>
2020-09-09 17:11:47 -0400

    Update September9-2020.js
    Manipulate Arrays With push()

--- September9-2020.js
+++ September9-2020.js
@@ -15,3 +15,13 @@ var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
 // myData should be equal to 8
 // first bracket is 2 cause it starts with 0 so 0 will be [1,2,3] and the second bracket is 1 cause 8 is place second from [7,8,9] 
 var myData = myArray[2][1];
+
+
+// Manipulate Arrays With push()
+
+var myArray = [["John", 23], ["cat", 2]];
+
+// equals to [["John", 23], ["cat", 2], ["dog", 3]];
+
+myArray.push(["dog", 3])
+
