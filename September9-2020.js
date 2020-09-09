15b1a250a50f1159aa65da1a416feb8d8e596930
crossar <41621244+crossar@users.noreply.github.com>
2020-09-09 17:06:59 -0400

    Update September9-2020.js
    Access Multi-Dimensional Arrays With Indexes

--- September9-2020.js
+++ September9-2020.js
@@ -7,3 +7,11 @@ var myArray = [9,10,28];
 
 myArray[0] = 45;
 console.log(myArray)
+
+// Access Multi-Dimensional Arrays With Indexes
+
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// myData should be equal to 8
+// first bracket is 2 cause it starts with 0 so 0 will be [1,2,3] and the second bracket is 1 cause 8 is place second from [7,8,9] 
+var myData = myArray[2][1];
