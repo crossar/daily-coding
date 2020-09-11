612690468ff143f0b75ce5835725e9ae61997420
crossar <41621244+crossar@users.noreply.github.com>
2020-09-11 18:18:47 -0400

    Create September11-2020.js
    Manipulate Arrays With shift()

---
+++ September11-2020.js
@@ -0,0 +1,11 @@
+// Manipulate Arrays With shift()
+var myArray = [["John", 23], ["dog", 3]];
+
+// This removed the first element in array
+// should be equals to  ["dog", 3]
+var removedFromMyArray = myArray.shift();
+console.log(myArray)
+
+
+
+
