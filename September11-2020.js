b56c9478779bf616d8bb0c642cac4f99c52a3582
crossar <41621244+crossar@users.noreply.github.com>
2020-09-11 18:23:51 -0400

    Update September11-2020.js
    Manipulate Arrays With unshift()

--- September11-2020.js
+++ September11-2020.js
@@ -6,6 +6,16 @@ var myArray = [["John", 23], ["dog", 3]];
 var removedFromMyArray = myArray.shift();
 console.log(myArray)
 
+// Manipulate Arrays With unshift()
+
+// This is similar to push() but instead of adding the element at the end of the array,unshift() adds the element at the beginning of the array.
+
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+myArray.unshift(["Paul",35]);
+console.log(myArray)
+
 
 
 
