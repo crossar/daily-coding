f48a0bcae09c948f5a350095af182e57b166154b
crossar <41621244+crossar@users.noreply.github.com>
2020-09-14 16:56:27 -0400

    Update September14-2020.js
    Global Scope and Functions

--- September14-2020.js
+++ September14-2020.js
@@ -6,4 +6,22 @@ function functionWithArgs(a,b) {
     functionWithArgs(1,2); 
     functionWithArgs(7,9);
 
-    
+// Global Scope and Functions
+
+// Declare your variable here
+var myGlobal = 10;
+
+function fun1() {
+  oopsGlobal = 5;
+}
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
