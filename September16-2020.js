bed3da08fa89bb148f46dcec13bd206aeabbdcb1
crossar <41621244+crossar@users.noreply.github.com>
2020-09-16 19:13:37 -0400

    Create September16-2020.js
    Understanding Undefined Value returned from a Function

---
+++ September16-2020.js
@@ -0,0 +1,17 @@
+// Understanding Undefined Value returned from a Function
+
+var sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+
+function addFive() {
+   sum = sum + 5;
+}
+
+addThree();
+addFive();
+
+
