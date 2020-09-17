5ae7855175cee402151a232cdde83b2c3df822a5
crossar <41621244+crossar@users.noreply.github.com>
2020-09-17 17:10:41 -0400

    Update September17-2020.js
    Comparison with the Strict Equality Operator

--- September17-2020.js
+++ September17-2020.js
@@ -17,7 +17,7 @@ function trueOrFalse(wasThatTrue) {
 
   // Comparison with the Equality Operator
 
-  
+
 function testEqual(val) {
     if (val == 12) { 
       return "Equal";
@@ -26,3 +26,17 @@ function testEqual(val) {
   }
   
   testEqual(10);
+
+  // Comparison with the Strict Equality Operator
+
+  // val is strictly equal to 7
+  
+function testStrict(val) {
+    if (val === 7) { 
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(10);
+  
