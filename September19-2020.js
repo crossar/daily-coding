72e9ddb5ad548f1c620309f41c812735193a6cc5
crossar <41621244+crossar@users.noreply.github.com>
2020-09-19 23:06:32 -0400

    Update September19-2020.js
    Comparisons with the Logical And Operator

--- September19-2020.js
+++ September19-2020.js
@@ -29,3 +29,16 @@ function testLessThan(val) {
   }
   
   testLessOrEqual(10);
+
+  // Comparisons with the Logical And Operator
+
+  function testLogicalAnd(val) {
+   
+  
+    if (val <= 50 && val >=25) {
+        return "Yes";
+    }
+    return "No";
+  }
+  
+  testLogicalAnd(10);
