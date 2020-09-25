146cb7f1a2a190dcb14e05d26bfb45857812769b
crossar <41621244+crossar@users.noreply.github.com>
2020-09-25 18:17:12 -0400

    September24$25-2020.js
    Logical Order in If Else Statements

---
+++ September24&25-2020.js
@@ -0,0 +1,14 @@
+// I forgot to do a check in last night cause I wasn't home. 
+
+// Logical Order in If Else Statements
+
+function orderMyLogic(val) {
+    if(val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+
+    
