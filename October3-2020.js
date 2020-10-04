cab056a8525aabc01e899cf82815d0d35e69cdb4
crossar <41621244+crossar@users.noreply.github.com>
2020-10-03 22:21:42 -0400

    Update October3-2020.js
    Accessing Object Properties with Bracket Notation

--- October3-2020.js
+++ October3-2020.js
@@ -12,7 +12,7 @@ var myDog = {
 
     // Accessing Object Properties with Dot Notation
 
-    // Setup
+   
     var testObj = {
     "hat": "ballcap",
     "shirt": "jersey",
@@ -23,3 +23,14 @@ var myDog = {
      var hatValue = testObj.hat;      
     var shirtValue = testObj.shirt;    
   
+// Accessing Object Properties with Bracket Notation
+
+var testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  
+  var entreeValue = testObj["an entree"];
+  var drinkValue = testObj["the drink"];
