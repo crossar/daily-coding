f82e61628b6e576f359b38312cd760ba186e43f0
crossar <41621244+crossar@users.noreply.github.com>
2020-10-03 22:12:27 -0400

    Create October3-2020.js
    Javascript Objects

---
+++ October3-2020.js
@@ -0,0 +1,13 @@
+// Build JavaScript Objects
+
+var myDog = {
+    // Only change code below this line
+    "name": "Peter",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Zoe", "Gravy"]
+    
+    // Only change code above this line
+    };
+
+    
