366cdcb963750004394883263c8d7791e573479b
crossar <41621244+crossar@users.noreply.github.com>
2020-10-06 21:34:29 -0400

    Create October6-2020.js
    Updating Object Properties

---
+++ October6-2020.js
@@ -0,0 +1,11 @@
+// Updating Object Properties
+
+var myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+
+  myDog.name = "Happy Coder";
