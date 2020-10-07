fa47834bce366c56cbe9f9cbd40d3f9145914e2a
crossar <41621244+crossar@users.noreply.github.com>
2020-10-06 21:36:02 -0400

    Update October6-2020.js
    Add New Properties to a JavaScript Object

--- October6-2020.js
+++ October6-2020.js
@@ -9,3 +9,15 @@ var myDog = {
   
 
   myDog.name = "Happy Coder";
+
+  // Add New Properties to a JavaScript Object
+
+  
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog.bark = "woof"
