b6cb601a2ea95539ec045f1e5997c5a75a95aa95
crossar <41621244+crossar@users.noreply.github.com>
2020-10-11 19:37:34 -0400

    Update October11-2020.js
    Manipulating Complex Objects

--- October11-2020.js
+++ October11-2020.js
@@ -7,3 +7,22 @@ function checkObj(obj, checkProp) {
       return "Not Found";
     }
   }
+
+  // Manipulating Complex Objects
+
+  var myMusic = [
+    {
+      artist: "Black Pink ft. Selena Gomez",
+      title: "Ice cream",
+      release_year: 2020,
+      formats: ["MP3", "MP4", "CD"],
+      gold: true
+    },
+    // record here
+    {
+      artist: "BTS",
+      title: "Dynamite",
+      release_year: 2020,
+      formats: ["CD", "MP3", "MP4"]
+    }
+  ];
