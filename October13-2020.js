a5a79cdf9b52a4d0f183626a815ca8e87d956244
crossar <41621244+crossar@users.noreply.github.com>
2020-10-13 15:09:14 -0400

    Create October13-2020.js
    Accessing Nested Objects

---
+++ October13-2020.js
@@ -0,0 +1,15 @@
+// Accessing Nested Objects
+
+var myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  var gloveBoxContents = myStorage.car.inside["glove box"]
