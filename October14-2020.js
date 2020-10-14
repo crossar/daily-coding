2bab9eb0bce69840d67c699ace509ec668683085
crossar <41621244+crossar@users.noreply.github.com>
2020-10-14 15:16:04 -0400

    Update October14-2020.js
     Iterate with JavaScript For Loops

--- October14-2020.js
+++ October14-2020.js
@@ -43,3 +43,15 @@ while(i >= 0) {
   myArray.push(i);
   i--;
 }
+
+// Iterate with JavaScript For Loops
+
+var ourArray = [];
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+var myArray = [];
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
