2eba1ec1e7f8057ee71576edca24765f050b9a0e
crossar <41621244+crossar@users.noreply.github.com>
2020-10-16 16:16:44 -0400

    Update October17-2020.js
     Count Backwards With a For Loop

--- October17-2020.js
+++ October17-2020.js
@@ -3,14 +3,25 @@
 var ourArray = [];
 
 for (var i = 0; i < 10; i += 2) {
-    ourArray.push(i);
-  }
-  
-  var myArray = [];
+  ourArray.push(i);
+}
 
-  
-  for (var i = 1; i < 10; i += 2) {
-    myArray.push(i);
-  }
+var myArray = [];
 
-  // 
+for (var i = 1; i < 10; i += 2) {
+  myArray.push(i);
+}
+
+// Count Backwards With a For Loop
+
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+var myArray = [];
+
+for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
